d179c15fafa13db833f06f80f2eb50cc4f218563
Faj5097 <fab.jan@t-online.de>
2022-10-19 14:21:43 +0200

    fixed item handler logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import { useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import CartProvider from "./store/CartProvider";
-import Cart from "./components/Cart/Cart";
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
-  function showCartHandler(){
+  const showCartHandler = () => {
     setCartIsShown(true);
-  }
+  };
 
-  function hideCartHandler(){
+  const hideCartHandler = () => {
     setCartIsShown(false);
-  }
-
+  };
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler}/>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
