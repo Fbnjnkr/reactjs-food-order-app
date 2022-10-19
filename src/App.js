a2183e2e64688bfa3cbe167a1a39fe0afdfbddd3
Fbnjnkr <fabian.janker@volkswagen.de>
2022-10-19 12:52:35 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  return (
+    <CartProvider>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
